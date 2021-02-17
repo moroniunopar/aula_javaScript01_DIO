80aa44be7fec69a58d55a309a398bb0fe823953a
moroniunopar <45543104+moroniunopar@users.noreply.github.com>
2021-02-17 12:52:54 -0300

    primeiro cmmt

---
+++ javaScript/comandos.js
@@ -0,0 +1,11 @@
+function trocar(elemento){
+  elemento.innerHTML = "ACHOU !!!";
+}
+
+function voltar(elemento){
+  elemento.innerHTML = "Cade o mouse??"
+}
+
+function clicou(elemento){
+  alert(elemento.innerHTML);
+}
